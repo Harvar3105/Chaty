6da03fa08e53d67ca36f54f1e04aa97b96c9510b
Harvar3105 <Petrochenko03@inbox.ru>
2024-10-23 21:37:10 +0300

    Uow now uses LazyLoad

--- DAL/UOW.cs
+++ DAL/UOW.cs
@@ -4,17 +4,26 @@ using MongoDbGenericRepository;
 
 namespace DAL;
 
-public class Uow(IMongoDbContext ctx) : BaseUow<IMongoDbContext>(ctx)
+public class Uow : BaseUow<IMongoDbContext>, IDisposable
 {
-    private ChatRepository? _chatRepository;
-    public ChatRepository ChatRepository => _chatRepository ?? new ChatRepository(Ctx, "Chat");
+    private readonly Lazy<ChatRepository> _chatRepository;
+    public ChatRepository ChatRepository => _chatRepository.Value;
 
-    private MessageRepository? _messageRepository;
-    public MessageRepository MessageRepository => _messageRepository ?? new MessageRepository(Ctx, "Message");
-    
+    private readonly Lazy<MessageRepository> _messageRepository;
+    public MessageRepository MessageRepository => _messageRepository.Value;
 
-    private RefreshTokenRepository? _refreshTokenRepository;
+    private readonly Lazy<RefreshTokenRepository> _refreshTokenRepository;
+    public RefreshTokenRepository RefreshTokenRepository => _refreshTokenRepository.Value;
 
-    public RefreshTokenRepository RefreshTokenRepository =>
-        _refreshTokenRepository ?? new RefreshTokenRepository(Ctx, "RefreshToken");
+    public Uow(IMongoDbContext ctx) : base(ctx)
+    {
+        _chatRepository = new Lazy<ChatRepository>(() => new ChatRepository(Ctx, "Chat"));
+        _messageRepository = new Lazy<MessageRepository>(() => new MessageRepository(Ctx, "Message"));
+        _refreshTokenRepository = new Lazy<RefreshTokenRepository>(() => new RefreshTokenRepository(Ctx, "RefreshToken"));
+    }
+
+    public void Dispose()
+    {
+        (Ctx as IDisposable)?.Dispose();
+    }
 }
